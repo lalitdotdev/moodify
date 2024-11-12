cb967522f52da5cfa878e6af46d224825af518dd
lalit sharma <geekymonkcp742k@gmail.com>
2024-11-12 15:12:51 +0530

    integrate sonner toast notifications.

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { Toaster as Sonner } from "sonner"
+import { useTheme } from "next-themes"
+
+type ToasterProps = React.ComponentProps<typeof Sonner>
+
+const SonnerToaster = ({ ...props }: ToasterProps) => {
+    const { theme = "system" } = useTheme()
+
+    return (
+        <Sonner
+            theme={theme as ToasterProps["theme"]}
+            className="toaster group"
+            toastOptions={{
+                classNames: {
+                    toast:
+                        "group toast group-[.toaster]:bg-purple-500/10 group-[.toaster]:text-blue-600 group-[.toaster]:border-slate-700 group-[.toaster]:shadow-lg group-[.toaster]:backdrop-blur-2xl group-[.toaster]:rounded-xl group-[.toaster]:text-blue-400",
+                    description: "group-[.toast]:text-blue-600",
+                    icon: "group-[.toast]:text-blue-600",
+                    actionButton:
+                        "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+                    cancelButton:
+                        "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+                },
+            }}
+            {...props}
+        />
+    )
+}
+
+export { SonnerToaster }
