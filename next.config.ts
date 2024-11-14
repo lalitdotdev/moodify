20b35520ba793a97c3c9f73918e7aac1d2f20c1d
lalit sharma <geekymonkcp742k@gmail.com>
2024-11-14 11:30:48 +0530

    add some fixes

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [{ key: 'Cache-Control', value: 'no-store' }],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
